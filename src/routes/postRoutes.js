5265a3d7b4745803595eb653eddacc2a0d6e29a5
Douglas Martins <douglascm19@gmail.com>
2024-11-23 11:00:42 -0300

    armazenamento e upload de imagens

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const upload = multer({dest:"./uploads"});
 
 const routes = (app) => {
     //Permite que o servidor interprete requisições com corpo no formato JSON
     app.use(express.json());
     //Rota para buscar todos os posts
     app.get("/posts", listarPosts);
+    //Rota para criar um post
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
 export default routes;
